687830ba1ad49c85aa27fbbd579a021e645b7593
Anniejs <anniejs@163.com>
2018-11-30 23:55:59 +0800

    比价产品渲染完成

--- js/savedetail.js
+++ js/savedetail.js
@@ -1,3 +1,17 @@
-$(function(){
-  
+$(function () {
+  var productid = tools.getParam("productid");
+  console.log(productid);
+  $.ajax({
+    type: "get",
+    url: "http://127.0.0.1:9090/api/getmoneyctrlproduct",
+    data: {
+      productid: productid
+    },
+    dataType: "json",
+    success: function (info) {
+      console.log(info);
+      var htmlStr = template("saveTmp",info);
+      $(".savecontent").html(htmlStr);
+    }
+  })
 })
